2ab31184c0217e90dfda95be994df50c426d4007
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-10 01:04:39 +0530

    add function to generate SHA-256 hash with specified prefix

---
+++ assignments2.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+function generateHashWithPrefixZero(prefix){
+    let input=0;
+    while(true){
+        let inputstr = 'abcd'+input.toString();
+        let hash =crypto.createHash('sha256').update(inputstr).digest('hex');
+        if(hash.startsWith(prefix)){
+            return {input : inputstr, hash : hash};
+        }
+        input++;
+    }
+}
+const result = generateHashWithPrefixZero('00000');
+console.log(`Input : ${result.input} , Hash : ${result.hash}`);
