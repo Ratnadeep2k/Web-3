1d57f5d2d2058106d8554ae7ba1a56815326efa2
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-16 20:52:59 +0530

    add bytesToAscii function to convert byte arrays to ASCII strings

---
+++ bytesToAscii.js
@@ -0,0 +1,7 @@
+function bytesToAscii(byteArray){
+    return new TextDecoder().decode(byteArray);
+}
+
+const bytes = new Uint8Array([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]);
+const ascii = bytesToAscii(bytes);
+console.log(ascii);
