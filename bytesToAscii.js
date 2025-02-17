d94c5996d745d5432bfcfa5b8ddc794cf84c832c
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-17 23:07:10 +0530

    add base64Encode function to encode Uint8Array to base64 and clean up bytesToAscii

--- bytesToAscii.js
+++ bytesToAscii.js
@@ -1,7 +1,6 @@
 function bytesToAscii(byteArray){
     return new TextDecoder().decode(byteArray);
 }
-
 const bytes = new Uint8Array([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]);
 const ascii = bytesToAscii(bytes);
 console.log(ascii);
